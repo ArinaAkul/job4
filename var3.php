27bc9da4751bca6255e6e3dc8299b25b43c5559c
ArinaAkul <arinaakulova66@gmail.com>
2020-12-08 21:21:51 +0300

    new

---
+++ var3.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $a=($b = 4) +5;
+    echo $a;
+    echo "<br\n>";
+    $d+=5;
+    echo $d;
+    echo "<br\n>";
+    $b="Hello";
+    echo $b;
+   ?>
+   <?php
+   $a = 5
+    echo $a . '<br>';
+$a+=2
+echo $a . '<br>';
+$a-=3
+echo $a . '<br>';
+
+    ?>
+</body>
+
+</html>
